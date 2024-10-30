2d908477417c2d8e6b1bd0d910876303835cde60
eduval <educristo@gmail.com>
2024-10-29 20:17:30 -0700

    First commit

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBbB64BMX1ZbiP-NBhapQBwUXFFlA7meu4",
+    authDomain: "ist105midtermexam-washingtonv.firebaseapp.com",
+    projectId: "ist105midtermexam-washingtonv",
+    storageBucket: "ist105midtermexam-washingtonv.appspot.com",
+    messagingSenderId: "512568613965",
+    appId: "1:512568613965:web:f97e344a023a492a375004"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
